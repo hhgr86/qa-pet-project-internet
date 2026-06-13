f91c613282b56d97ca7d8fdd87e37764ddb2dc93
hhgr86 <106955659+hhgr86@users.noreply.github.com>
2026-06-13 12:37:57 +0300

    Add files via upload

---
+++ PetAutoTests/src/test/java/CheckBoxTests.java
@@ -0,0 +1,50 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class CheckBoxTests {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    @DisplayName("BUG-003: Второй чекбокс самовольно возвращается после перезагрузки")
+    public void testSecondCheckboxReturnsToChecked() {
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+
+        WebElement secondCheckbox = driver.findElement(By.xpath("//input[2]"));
+
+        if (secondCheckbox.isSelected()) {
+            secondCheckbox.click();
+            System.out.println("Сняли галочку со второго чекбокса");
+        }
+
+        Assertions.assertFalse(secondCheckbox.isSelected(), "Галочка должна быть снята");
+
+        driver.navigate().refresh();
+
+        WebElement secondCheckboxAfterRefresh = driver.findElement(By.xpath("//input[2]"));
+        boolean stateAfterRefresh = secondCheckboxAfterRefresh.isSelected();
+
+        Assertions.assertFalse(stateAfterRefresh, 
+            "BUG-003: Второй чекбокс должен оставаться НЕотмеченным после перезагрузки!");
+    }
+}
